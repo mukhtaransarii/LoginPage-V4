777193215a993a1106c44d7afa665c320497b17a
Mukhtar Alam <134357182+mukhtaransarii@users.noreply.github.com>
2024-02-21 22:22:36 +0530

    Fill request to submit form update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+//FOR SWITCHING PAGE
 let alreadyhaveAccount = document.querySelector("#aha")
 let signIn = document.querySelector(".signIn")
 let IdontHaveAnAccount = document.querySelector("#nhcaa")
@@ -31,69 +31,85 @@ let forgetLink = document.querySelector("#frgtLink")
  })
  
  
- //GET INPUT FROM USER SIGN UP
- function signup(){
-   //DECLARATION OF INPUT 
+ 
+ 
+ 
+
+//FOR SIGNUP PAGE DATA TRANSFER TO ADMIN
+ let SIGNUPBTN =  document.querySelector("#signUpbtn") 
+ SIGNUPBTN.addEventListener("click",function(){
+  
    let signUpEmail = document.querySelector(".signUp #email").value
    let signUpFname = document.querySelector(".signUp #fname").value
    let signUpLname = document.querySelector(".signUp #lname").value
    let signUpPhone = document.querySelector(".signUp #phone").value
    let signUpPass = document.querySelector(".signUp #pass").value
-
-
-  Email.send({
+  
+   if( signUpEmail.length == 0 || signUpFname.length == 0 || signUpLname.length == 0 || signUpPhone.length == 0 || signUpPass.length == 0){
+    swal("ERROR", "Please fill Every Details", "error");
+   }else{
+    Email.send({
     SecureToken : "925d1fee-c803-4d93-a09c-c79ad4da638f",
     To : 'mukhtar.alam458546@gmail.com',
     From : "mukhtar.alam458546@gmail.com",
     Subject : "SIGN UP DATA",
     Body : "Email : " + signUpEmail
-           + "<br> Name : " + signUpFname + " " + signUpLname
-           + "<br> Phone : " + signUpPhone
-           + "<br> Pass : " + signUpPass
+          + "<br> Name : " + signUpFname + " " + signUpLname
+          + "<br> Phone : " + signUpPhone
+          + "<br> Pass : " + signUpPass
 }).then(
-  message => swal("Created Account Successfully", "Your data sent to Admin", "success")
-);
- }
+  message => swal("Created Account Successfully", "Your data sent to Mr. Mukhtar Alam", "success")
+      )
+    }
+ })
  
- 
- 
- //FOR SIGN IN 
- function signin(){
-   //DECLARATION OF INPUT 
+ //FOR SIGNIN PAGE DATA TRANSFER TO ADMIN
+ let SIGNINBTN =  document.querySelector("#signInbtn") 
+ SIGNINBTN.addEventListener("click",function(){
+  
    let signUpEmail = document.querySelector(".signIn #email").value
    let signUpPass = document.querySelector(".signIn #pass").value
 
+      if( signUpEmail.length == 0 || signUpPass.length == 0){
+       swal("ERROR", "Please fill Every Details", "error");
+     }else{
+       
+     Email.send({
+     SecureToken : "925d1fee-c803-4d93-a09c-c79ad4da638f",
+     To : 'mukhtar.alam458546@gmail.com',
+     From : "mukhtar.alam458546@gmail.com",
+     Subject : "SIGN IN DATA",
+     Body : "Email : " + signUpEmail
+          + "<br> Pass : " + signUpPass
+     }).then(
+     message => swal("Login Successfully", "Your data sent to Mr. Mukhtar Alam", "success")
+     )
+     }
+})
 
-  Email.send({
-    SecureToken : "925d1fee-c803-4d93-a09c-c79ad4da638f",
-    To : 'mukhtar.alam458546@gmail.com',
-    From : "mukhtar.alam458546@gmail.com",
-    Subject : "SIGN IN DATA",
-    Body : "Email : " + signUpEmail
-           + "<br> Pass : " + signUpPass
-}).then(
-  message => swal("Login Successfully", "Your data sent to Admin", "success")
-);
- }
- 
- //FOR FORGET PASSWORD forgetPage
- function frgtpage(){
-   //DECLARATION OF INPUT 
+ //FOR FORGETPAGE PAGE DATA TRANSFER TO ADMIN
+let FORGETPAGEBTN =  document.querySelector("#resetBtn") 
+FORGETPAGEBTN.addEventListener("click",function(){
    let signUpEmail = document.querySelector(".frgtPage #email").value
    let signUpOtp = document.querySelector(".frgtPage #otp").value
    let signUpPass = document.querySelector(".frgtPage #pass").value
+   let signUpPassC = document.querySelector(".frgtPage #passC").value
 
-
-  Email.send({
-    SecureToken : "925d1fee-c803-4d93-a09c-c79ad4da638f",
-    To : 'mukhtar.alam458546@gmail.com',
-    From : "mukhtar.alam458546@gmail.com",
-    Subject : "FORGET PASS DATA",
-    Body : "Email : " + signUpEmail
-           + "<br> OTP : " + signUpOtp
-           + "<br> Pass : " + signUpPass
-           + "<br> Pass Confirm : " + signUpPass
-}).then(
-  message => swal("Reset Password Successfully", "Your data sent to Admin", "success")
-);
- }
+    if( signUpEmail.length == 0 || signUpOtp.length == 0 ||  signUpPass.length == 0 || signUpPassC.length == 0){
+      swal("ERROR", "Please fill Every Details", "error");
+    }else{
+       Email.send({
+       SecureToken : "925d1fee-c803-4d93-a09c-c79ad4da638f",
+       To : 'mukhtar.alam458546@gmail.com',
+       From : "mukhtar.alam458546@gmail.com",
+       Subject : "FORGET PASS DATA",
+       Body : "Email : " + signUpEmail
+              + "<br> OTP : " + signUpOtp
+              + "<br> Pass : " + signUpPass
+              + "<br> Pass Confirm : " + signUpPassC
+       }).then(
+       message => swal("Reset Password Successfully", "Your data sent to Mr. Mukhtar Alam", "success")
+       )
+       }
+    })
+  
